d035bb8c5f7e47a1656d5a9397403a8514fa54c6
82104 <skl509@naver.com>
2021-01-08 23:49:52 +0900

    01

---
+++ src/com/study/java/ch03/ex01/Main.java
@@ -0,0 +1,8 @@
+package ch03.ex01;
+
+public class Main {
+    public static void main(String[] args){
+    Employee employee = new Employee();
+    System.out.println("직원 배열의 평균 급여는 " +employee.average() + "입니다");
+    }
+}
---
+++ src/com/study/java/ch03/ex01/Measurable.java
@@ -0,0 +1,6 @@
+package ch03.ex01;
+
+public interface Measurable {
+    double getMeasure();
+    int average(); // 인터페이스는 설계할 메소드만 있어야된다
+}
