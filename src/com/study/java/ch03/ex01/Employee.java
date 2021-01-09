acc44b4570b0ea881a677a2addf289f44c0caae8
82104 <skl509@naver.com>
2021-01-09 15:23:57 +0900

    3-01 확인용2

--- src/com/study/java/ch03/ex01/Employee.java
+++ src/com/study/java/ch03/ex01/Employee.java
@@ -1,8 +1,18 @@
 package com.study.java.ch03.ex01;
 
 public class Employee implements Measurable {
+    double pay;
+
+
     @Override
     public double getMeasure() {
-        return 0;
+        return this.pay;
+
     }
+
+    public void setMeasure(double pay, String[] name) {
+        this.pay = pay;
+
+    }
+
 }
--- src/com/study/java/ch03/ex01/Main.java
+++ src/com/study/java/ch03/ex01/Main.java
@@ -1,12 +1,47 @@
 package com.study.java.ch03.ex01;
 
+import java.util.List;
+import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args){
-        Employee employee = new Employee();
-        System.out.println("직원 배열의 평균 급여는 " +average(...) + "입니다");
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        List<String> list = new ArrayList<>();
+        list.add("a씨");
+        list.add("b씨");
+        list.add("c씨");
+        list.add("d씨");
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+
+
     }
 
+
+
     public static double average(Measurable[ ] objects) {
-        return 0;
+
+        setAverage(this.line0/4); // 오류나는 이유를 잘 모름
+        return this;
     }
+
+
+
+
+    public static void setAverage(double Measurable[],double pay){
+        for(int i=0; i<4; i++) {
+        }
+        double line0 = Measurable[0] + Measurable[1] + Measurable[2] + Measurable[3];
+        Measurable[0] = pay;
+        Measurable[1] = pay;
+        Measurable[2] = pay;
+        Measurable[3] = pay;
+    }
+
+
+
+
+
 }
