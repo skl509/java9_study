1b1255bfc24a77c5ea2fd8fa2a7bb39fe70ecae3
82104 <skl509@naver.com>
2021-03-12 00:40:13 +0900

    9예제1

---
+++ src/com/study/java/ch09/practice05/Person.java
@@ -0,0 +1,25 @@
+package com.study.java.ch09.practice05;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    private Object writeReplace() {
+        return new PersonProxy(id);
+    }
+    
+    public String toString() {
+        return String.format("%s[id=%d,name=%s]", getClass().getName(), id, name);
+    }
+}
---
+++ src/com/study/java/ch09/practice05/PersonDatabase.java
@@ -0,0 +1,20 @@
+package com.study.java.ch09.practice05;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public enum PersonDatabase {
+    INSTANCE;
+    
+    private Map<Integer, Person> people = new ConcurrentHashMap<>(); 
+    private PersonDatabase() {
+        add(new Person(1, "Adam"));
+        add(new Person(2, "Eve"));
+    }
+    
+    public Person findById(int id) { return people.get(id); }
+    
+    public void add(Person p) {
+        people.putIfAbsent(p.getId(), p);
+    }
+}
